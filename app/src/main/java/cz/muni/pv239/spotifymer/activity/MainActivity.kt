56c43070e70bbf33e923f179f3f7bfecfe5b3b63
Miroslav Mikolaj <469508@mail.muni.cz>
2020-03-15 18:09:33 +0100

    connecting to Spotify api added

--- app/src/main/java/cz/muni/pv239/spotifymer/activity/MainActivity.kt
+++ app/src/main/java/cz/muni/pv239/spotifymer/activity/MainActivity.kt
@@ -1,25 +1,35 @@
 package cz.muni.pv239.spotifymer.activity
 
+import android.app.ListActivity
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.google.android.material.textview.MaterialTextView
 import com.spotify.android.appremote.api.ConnectionParams
+import com.spotify.android.appremote.api.Connector
+import com.spotify.android.appremote.api.SpotifyAppRemote
 import cz.muni.pv239.spotifymer.R
 import cz.muni.pv239.spotifymer.credentials.CLIENT_ID
 import cz.muni.pv239.spotifymer.credentials.REDIRECT_URI
+import cz.muni.pv239.spotifymer.credentials.SPOTIFY_APP_REMOTE
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        findViewById<FloatingActionButton>(R.id.floating_action_button).setOnClickListener {
+            this.test()
+        }
     }
 
     override fun onStart() {
         super.onStart()
-        val connectionParams = ConnectionParams.Builder(CLIENT_ID)
-            .setRedirectUri(REDIRECT_URI)
-            .showAuthView(true)
-            .build()
+
+
+
     }
 
     private fun connected() {
@@ -29,5 +39,31 @@ class MainActivity : AppCompatActivity() {
     override fun onStop() {
         super.onStop()
         // Aaand we will finish off here.
+    }
+
+
+    private fun test() {
+        val textView = findViewById<MaterialTextView>(R.id.connect_update)
+
+        val connectionParams = ConnectionParams.Builder(CLIENT_ID)
+            .setRedirectUri(REDIRECT_URI)
+            .showAuthView(true)
+            .build()
+
+        SpotifyAppRemote.connect(this, connectionParams, object : Connector.ConnectionListener {
+            override fun onConnected(appRemote: SpotifyAppRemote) {
+                SPOTIFY_APP_REMOTE = appRemote
+                textView.text = "CONNECTED"
+                Log.d("MainActivity", "Connected! Yay!")
+                // Now you can start interacting with App Remote
+                connected()
+            }
+
+            override fun onFailure(throwable: Throwable) {
+                textView.text = "FAILURE"
+                Log.e("MainActivity", throwable.message, throwable)
+                // Something went wrong when attempting to connect! Handle errors here
+            }
+        })
     }
 }
